d64506c6b4367a103761d3f04bd4f82e6a46d63e
Igor Kioresko <ikioresko@gmail.com>
2021-09-15 14:16:09 +0500

    4.1. JSON формат. [#238554]

---
+++ src/main/java/ru/job4j/dream/servlets/Email.java
@@ -0,0 +1,13 @@
+package ru.job4j.dream.servlets;
+
+public class Email {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
